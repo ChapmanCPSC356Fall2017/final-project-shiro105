35f89a193df29a25d43708dbc7baa0b24ec572dd
shiro105 <shiro105@mail.chapman.edu>
2017-12-01 18:03:11 -0800

    Added in layouts for Cell, viewing and editing a character

--- CharacterPicker/app/src/main/java/cpsc356/characterpicker/Activities/MainActivity.java
+++ CharacterPicker/app/src/main/java/cpsc356/characterpicker/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package cpsc356.characterpicker;
+package cpsc356.characterpicker.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cpsc356.characterpicker.R;
 
 public class MainActivity extends AppCompatActivity {
 
